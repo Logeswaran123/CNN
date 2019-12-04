adcf1c5024325d5211a27c98aaf60833f884d0ea
Logeswaran Sivakumar <36563521+Logeswaran123@users.noreply.github.com>
2019-12-04 09:48:09 +0530

    Add files via upload

---
+++ Text Generation - LSTM/clean.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 11:48:12 2019
+
+@author: admin
+"""
+
+import string
+
+
+# save tokens to file
+def save_doc(lines, filename):
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+
+# load document
+filename = 'soap.txt'
+file = open(filename, 'r')
+# read all text
+doc = file.read()
+# close the file
+file.close()
+print(doc[:200])
+
+# clean the document
+# replace '-' & '--' with a space ' '
+doc = doc.replace('--', ' ')
+doc = doc.replace('-', ' ')
+# split into tokens by white space
+tokens = doc.split()
+# remove punctuation from each token
+table = str.maketrans('', '', string.punctuation)
+tokens = [w.translate(table) for w in tokens]
+# remove non alphabetic tokens
+tokens = [word for word in tokens if word.isalpha()]
+# make all as lower case
+tokens = [word.lower() for word in tokens]
+print('TOTAL TOKENS: %d' % len(tokens))
+print('UNIQUE TOKENS: %d' % len(set(tokens)))
+
+# organize into sequences of tokens
+length = 50 + 1
+sequences = list()
+for i in range(length, len(tokens)):
+	# select sequence of tokens
+	seq = tokens[i-length:i]
+	# convert into a line
+	line = ' '.join(seq)
+	# store the line
+	sequences.append(line)
+print('TOTAL SEQUENCES: %d' % len(sequences))
+
+# save sequences to file
+output_file = 'cleaned.txt'
+save_doc(sequences, output_file)
