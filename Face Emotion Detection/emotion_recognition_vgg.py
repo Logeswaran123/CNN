aec6131f71304f6837bd693322c06aa0ca9d600a
Logeswaran Sivakumar <36563521+Logeswaran123@users.noreply.github.com>
2019-12-13 16:06:36 +0530

    Add files via upload

---
+++ Face Emotion Detection/emotion_recognition_vgg.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 21:36:03 2019
+
+@author: admin
+"""
+
+import sys, os
+import pandas as pd
+import numpy as np
+
+from keras.models import Sequential,Model,load_model
+from keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization,AveragePooling2D
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras import applications
+from keras.utils import np_utils
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report, confusion_matrix
+from matplotlib import pyplot as plt
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
+
+df=pd.read_csv('fer2013.csv')
+
+# print(df.info())
+# print(df["Usage"].value_counts())
+
+# print(df.head())
+X_train,train_y,X_test,test_y=[],[],[],[]
+
+for index, row in df.iterrows():
+    val=row['pixels'].split(" ")
+    try:
+        if 'Training' in row['Usage']:
+           X_train.append(np.array(val,'float32'))
+           train_y.append(row['emotion'])
+        elif 'PublicTest' in row['Usage']:
+           X_test.append(np.array(val,'float32'))
+           test_y.append(row['emotion'])
+    except:
+        print(f"error occured at index :{index} and row:{row}")
+
+
+num_features = 64
+num_labels = 7
+batch_size = 64
+epochs = 10
+width, height = 48, 48
+
+
+X_train = np.array(X_train,'float32')
+train_y = np.array(train_y,'float32')
+X_test = np.array(X_test,'float32')
+test_y = np.array(test_y,'float32')
+
+train_y=np_utils.to_categorical(train_y, num_classes=num_labels)
+test_y=np_utils.to_categorical(test_y, num_classes=num_labels)
+
+#cannot produce
+#normalizing data between oand 1
+X_train -= np.mean(X_train, axis=0)
+X_train /= np.std(X_train, axis=0)
+
+X_test -= np.mean(X_test, axis=0)
+X_test /= np.std(X_test, axis=0)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+base_model = applications.vgg19.VGG19(include_top=False, weights=None, input_shape= (height,width,1))
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.7)(x)
+predictions = Dense(num_labels, activation= 'softmax')(x)
+model = Model(inputs = base_model.input, outputs = predictions)
+
+
+#Compliling the model
+model.compile(loss=categorical_crossentropy,
+              optimizer=Adam(lr=0.0001),
+              metrics=['accuracy'])
+
+#Training the model
+history = model.fit(X_train, train_y,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(X_test, test_y),
+          shuffle=True)
+
+
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig('Accuracy.png')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig('Loss.png')
+plt.show()
+
+train_score = model.evaluate(X_train, train_y, verbose=0)
+print('Train loss:', train_score[0])
+print('Train accuracy:', 100*train_score[1])
+ 
+test_score = model.evaluate(X_test, test_y, verbose=0)
+print('Test loss:', test_score[0])
+print('Test accuracy:', 100*test_score[1])
+
+model.summary()
+
+#Saving the  model to  use it later on
+fer_json = model.to_json()
+with open("fer.json", "w") as json_file:
+    json_file.write(fer_json)
+model.save_weights("fer.h5")
+
